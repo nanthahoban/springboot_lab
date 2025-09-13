61f7ccb7c74e9064b8174a748c6a0c417319b132
pamma2004 <pamodithrathnaweera@gmail.com>
2025-09-13 17:03:33 +0530

    Springboot app

---
+++ src/main/java/com/example/demo/StuRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//Remove @RepositoryRestResource below to disable auto REST api:
+@RepositoryRestResource
+public interface StuRepo extends CrudRepository<Student, String>{}
+
